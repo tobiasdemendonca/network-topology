9c80b194fa32f97648b8b2780c2f3faa0d4cca61
Andrew Lamzed-Short <andrew.lamzedshort@canonical.com>
2025-05-15 10:23:03 +0200

    feat: YAML-ified the current POC

--- network.py
+++ network.py
@@ -1,5 +1,47 @@
+#!/usr/bin/env python3
 from __future__ import annotations
-import json
+import yaml
+from pathlib import Path
+import mmap
+
+class Node:
+    def __init__(self, system_id: str, hostname: str, ip: str, subnet: Subnet) -> None:
+        self.system_id = system_id
+        self.hostname = hostname
+        self.ip = ip
+        self.subnet = subnet
+    
+    def to_mermaid(self) -> str:
+        return f"{self.system_id}[Node_{self.system_id}<br>hostname={self.hostname}]"
+    
+    def __str__(self):
+        return f"Node(system_id={self.system_id}, hostname={self.hostname}, ip={self.ip}, subnet={self.subnet})"
+
+class Subnet:
+    def __init__(self, id: int, cidr: str) -> None:
+        self.id = id
+        self.cidr = cidr
+    
+    def to_mermaid(self) -> str:
+        # id() is used to get a unique identifier for the subnet
+        # as i don't know how to reformat the cidr nicely
+        return f"Subnet_{self.id}[Subnet_{self.id}<br>{self.cidr}]"
+    
+    def __str__(self):
+        return f"Subnet_{self.id}(cidr={self.cidr})"
+    
+    def __eq__(self, other: Subnet) -> bool:
+        if not isinstance(other, Subnet):
+            return False
+        return self.cidr == other.cidr
+
+    def __hash__(self) -> int:
+        return hash(self.cidr)
+
+    def __ne__(self, value):
+        if not isinstance(value, Subnet):
+            return True
+        return self.cidr != value.cidr
 
 class NetworkInterface: 
     def __init__(self, name: str, ipaddress: str, subnet: Subnet) -> None: 
@@ -10,94 +52,77 @@ class NetworkInterface:
     def __repr__(self) -> str:
         return f"NetworkInterface(name={self.name}, ipaddress={self.ipaddress}, subnet={self.subnet})"
 
-class Node: 
-    def __init__(self, name: str, interfaces: list[NetworkInterface]) -> None: 
-        self.name = name
-        self.interfaces = interfaces
-
-    @property
-    def name_without_spaces(self) -> str:
-        return self.name.replace(' ', '_')
-        
-    def __repr__(self) -> str:
-        return f"Node(name={self.name}, interfaces={self.interfaces})"
-
-class Subnet: 
-    def __init__(self, name: str, cidr: str) -> None:
-        self.name = name
-        self.cidr = cidr
-
-    @property
-    def name_without_spaces(self) -> str:
-        return self.name.replace(" ", "_")
-    
-    def __repr__(self) -> str:
-        return f"Subnet(name={self.name}, cidr={self.cidr})"
-    
 class Network:
-    def __init__(self, nodes: list[Node], subnets: list[Subnet]) -> None: 
+    def __init__(self, nodes: set[Node], subnets: set[Subnet]) -> None: 
         self.nodes = nodes
         self.subnets = subnets
         
     @classmethod
-    def from_json(cls, json_data: dict) -> Network: 
-        return Network(**json_data)
+    def from_yaml(cls, yaml_file: Path) -> Network:
+        known_subnets = set()
+        nodes = set()
 
-class Outputer:
-    
-    def _start_string(self) -> str:
-        return "graph TD\n\n"
-    
-    def output(self, network: Network) -> str:
-        out  = self._start_string()
+        subnet_id = 0
+
+        with open(yaml_file, "r") as f:
+            with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as mm:
+                data = yaml.safe_load(mm.read())
+                for k in data:
+                    sn = Subnet(0, data[k]["subnet"])
+                    if sn not in known_subnets:
+                        sn.id = subnet_id
+                        subnet_id += 1
+                        known_subnets.add(sn)
+                    n = Node(k, data[k]["hostname"], data[k]["ip"], sn)
+                    nodes.add(n)
+
+        # print("Subnets:")
+        # for sn in known_subnets:
+        #     print("\t", sn)
         
+        # print("Nodes:")
+        # for n in nodes:
+        #     print("\t", n)
+        
+        return Network(nodes, known_subnets)
+
+class MermaidOutputter:
+    @staticmethod
+    def construct_mermaid(network: Network) -> str:
+        output = "graph LR\n\n"
+
+        # Write out the subnets first as they're independent
         for subnet in network.subnets:
-            out += f"  {subnet.name_without_spaces}[\"{subnet.name}<br>{subnet.cidr}\"]\n"           
-        
-        out += "\n\n"
-        
-        for node in network.nodes:
-            out += f"  {node.name_without_spaces}[\"{node.name}\"]\n"
+            output += f"  {subnet.to_mermaid()}\n"
 
-        out += "\n\n"
-        
+        output += "\n"
+
+        # Nodes are in subnets to write them next
         for node in network.nodes:
-            for interface in node.interfaces:
-                out += f"  {node.name_without_spaces} -->|"
-                out += f"{interface.name}: {interface.ipaddress}"
-                out += f"| {interface.subnet.name_without_spaces}"
-                out += '\n'
-            out += '\n'
-            
-        return out
-        
+            output += f"  {node.to_mermaid()}\n"
+        output += "\n"
+
+        # Then finish by writing out the connections between nodes and their subnets
+        for node in network.nodes:
+            # TODO: Get network interface name
+
+            # TODO: Clean up getting a subnet
+            sn = None
+            for subnet in network.subnets:
+                if node.subnet == subnet:
+                    sn = subnet
+                    break
+            output += f"  {node.system_id} -->|{node.ip}| {sn.to_mermaid()}\n"
+
+        return output
 
 def main():
+    print(
+        MermaidOutputter.construct_mermaid(
+            Network.from_yaml("example-network.yaml")
+        )
+    )
     
-    subnet1 = Subnet(name="Subnet 1", cidr="192.168.1.0/24")
-    subnet2 = Subnet(name="Subnet 2", cidr="192.168.2.0/24")
-
-    interface1 = NetworkInterface(name="eth0", ipaddress="192.168.1.10", subnet=subnet1)
-    interface2 = NetworkInterface(name="eth1", ipaddress="192.168.1.11", subnet=subnet1)
-    interface3 = NetworkInterface(name="eth2", ipaddress="192.168.2.10", subnet=subnet2)
-
-    node1 = Node(name="Node 1", interfaces=[interface1, interface2, interface3])
-
-    subnet3 = Subnet(name="Subnet 3", cidr="192.168.3.0/24")
-    interface4 = NetworkInterface(name="eth3", ipaddress="192.168.3.10", subnet=subnet3)
-
-    node2 = Node(name="Node 2", interfaces=[interface4])
-    
-    network = Network(nodes=[node1, node2], subnets=[subnet1, subnet2, subnet3])
-    outputer = Outputer()
-    print(outputer.output(network))
-    
-    # with open("network.json") as f:
-    #     data = json.load(f)
-
-    #     network = Network.from_json(data)
-    #     outputer = Outputer()
-    #     outputer.output(network)
     
 if __name__ == "__main__":
     main()
